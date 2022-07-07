851187d63acb16188570a4468fc8fd3aa6ad74c1
Ayub <alamayub85@gmail.com>
2022-07-07 10:28:33 +0545

    photo gallery with firebase and flutter bloc completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_test/bloc/app_bloc.dart';
+import 'package:flutter_bloc_test/bloc/app_event.dart';
+import 'package:flutter_bloc_test/bloc/app_state.dart';
+import 'package:flutter_bloc_test/dialogs/show_auth_error.dart';
 import 'package:flutter_bloc_test/firebase_options.dart';
+import 'package:flutter_bloc_test/loading/loading_screen.dart';
 import 'package:flutter_bloc_test/photo_gallery_view.dart';
+import 'package:flutter_bloc_test/views/login_view.dart';
+import 'package:flutter_bloc_test/views/register_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,12 +25,45 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider<AppBloc>(
+      create: (_) => AppBloc()..add(const AppEventInitialize()),
+      child: MaterialApp(
+        title: 'Photo Gallery',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: BlocConsumer<AppBloc, AppState>(
+          listener: (context, appState) {
+            if (appState.isLoading) {
+              LoadingScreen.instance().show(
+                context: context,
+                text: 'Loading...',
+              );
+            } else {
+              LoadingScreen.instance().hide();
+            }
+            final authError = appState.authError;
+            if (authError != null) {
+              showAuthError(
+                authError: authError,
+                context: context,
+              );
+            }
+          },
+          builder: (context, appState) {
+            if (appState is AppStateLoggedOut) {
+              return const LoginView();
+            } else if (appState is AppStateLoggedIn) {
+              return const PhotoGalleryView();
+            } else if (appState is AppStateRegistrationView) {
+              return const RegisterView();
+            } else {
+              return Container();
+            }
+          },
+        ),
       ),
-      home: const PhotoGalleryView(),
     );
   }
 }
--- lib/photo_gallery_view.dart
+++ lib/photo_gallery_view.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_test/bloc/app_bloc.dart';
+import 'package:flutter_bloc_test/bloc/app_event.dart';
+import 'package:flutter_bloc_test/bloc/app_state.dart';
+import 'package:flutter_bloc_test/views/main_menu_popup.dart';
+import 'package:flutter_bloc_test/views/storage_image_view.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class PhotoGalleryView extends StatefulWidget {
+class PhotoGalleryView extends HookWidget {
   const PhotoGalleryView({Key? key}) : super(key: key);
 
   @override
-  State<PhotoGalleryView> createState() => _PhotoGalleryViewState();
-}
-
-class _PhotoGalleryViewState extends State<PhotoGalleryView> {
-  @override
   Widget build(BuildContext context) {
+    final picker = useMemoized(() => ImagePicker(), [key]);
+    final images = context.watch<AppBloc>().state.images ?? [];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AppBar'),
+        title: const Text('Photo Gallery'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final image = await picker.pickImage(source: ImageSource.gallery);
+              if (image == null) {
+                return;
+              }
+              // ignore: use_build_context_synchronously
+              context.read<AppBloc>().add(
+                    AppEventUploadImage(
+                      filePathToUpload: image.path,
+                    ),
+                  );
+            },
+            icon: const Icon(Icons.upload),
+          ),
+          const MainPopupMenuButton()
+        ],
       ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: GridView.count(
+        padding: const EdgeInsets.all(16.0),
+        shrinkWrap: true,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        crossAxisCount: 2,
+        children: images.map((img) => StorageImageView(image: img)).toList(),
       ),
     );
   }
